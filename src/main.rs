925f41f0ca8c90db8a31b14aad81a31cab146520
CNKommander <33795252+CNKommander@users.noreply.github.com>
2024-12-14 16:30:06 -0600

    implement prompt
    basic CLI prompt implemented

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::fs;
 
 use colorized::*;
 
+use inquire::{Text, Select};
+
 /// CLI tool for scaffolding an empty Star Wars: Empire at War: Forces of Corruption mod project
 #[derive(Parser)]
 #[clap(name = "SW EAW FoC Mod Generator")]
@@ -16,7 +18,31 @@ struct Cli {
     project_name: String,
 }
 
+fn run_prompt() {
+    let project_name_ans = Text::new("mod name: ")
+        .with_help_message("The name of the mod you want to create")
+        .prompt();
+
+    match project_name_ans {
+        Ok(project_name_ans) => colorize_print(format!("You entered: {project_name_ans} "), Colors::BrightGreenFg),
+        Err(_) => colorize_println("Error: Invalid mod name!", Colors::BrightRedFg),
+    }
+
+    let mod_options = vec!["Mod", "Submod"];
+
+    let options_ans = Select::new("mod type: ", mod_options)
+        .with_help_message("Select the type of mod you want to create: ")
+        .prompt();
+
+    match options_ans {
+        Ok(mod_options) => colorize_print(format!("You selected: {mod_options} "), Colors::BrightGreenFg),
+        Err(_) => colorize_println("Error: Invalid mod type!", Colors::BrightRedFg),
+    }
+        
+}
+
 fn main() {
+    run_prompt();
     let cli = Cli::parse();
     
     let project_path = Path::new(&cli.project_name);
@@ -29,7 +55,7 @@ fn main() {
     generate_directories(project_path);
     generate_readme(project_path, &cli.project_name);
 
-    colorize_println(&format!("Mod project {} created successfully!", cli.project_name), Colors::BrightGreenFg);
+    colorize_println(format!("Mod project {} created successfully!", cli.project_name), Colors::BrightGreenFg);
 }
 
 fn generate_directories(project_path: &Path) {
